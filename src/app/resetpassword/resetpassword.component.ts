ae915b8565d99c8adc66c23fbe1efe1ebef6700e
uhuru rawlings <rawlings.huru@student.moringaschool.com>
2022-04-03 12:16:49 +0300

    added rest passwordd databinding

--- src/app/resetpassword/resetpassword.component.ts
+++ src/app/resetpassword/resetpassword.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ResetpasswordComponent implements OnInit {
   useremail = ''
   password = ''
+  confirmpassword = ''
   constructor() { }
 
   ngOnInit(): void {
