5ba411ff8efe71808798a09e6f3080625fe665fe
Francis Githae <francis.githae@student.moringaschool.com>
2022-04-03 08:25:08 +0300

    add menu card

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-
+import { MenuService } from '../http-client/menu.service';
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
-
-  constructor() { }
+  menuList:any;
+  constructor(private menuService:MenuService) { }
 
   ngOnInit(): void {
+
+    this.menuService.getMenu().subscribe(data=>{
+
+      this.menuList=data
+    })
   }
 
 }
