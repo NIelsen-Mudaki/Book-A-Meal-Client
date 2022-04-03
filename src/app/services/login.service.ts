4592b346bdf267022eb08a093ec20390aa59db3a
uhuru rawlings <rawlings.huru@student.moringaschool.com>
2022-04-03 09:28:13 +0300

    added login service

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
-
-  constructor() { }
+  url = 'http://127.0.0.1:9000/api/login'
+  constructor(private http:HttpClient) { }
+  login(useremail:any, password:any):Observable<any>{
+    return this.http.get(this.url +"?useremail=" + useremail + "password=" + password)
+  }
 }
