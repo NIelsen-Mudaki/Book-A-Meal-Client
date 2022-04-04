4f0edcfe8408051f1cbd0d889f5ba11c70ca19b1
Nielsen-Mudaki <jumbanielsen@gmail.com>
2022-04-04 10:33:15 +0300

    add navbar and footer to all pages

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -5,7 +5,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class LoginService {
-  url = 'http://127.0.0.1:9000/api/login'
+  url = 'http://127.0.0.1:8000/api/login'
   constructor(private http:HttpClient) { }
   login(credentials:any):Observable<any>{
     return this.http.post<any>(this.url ,credentials)
