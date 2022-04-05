991466f1c6000519675f8596d07ea758d6e015ed
uhuru rawlings <rawlings.huru@student.moringaschool.com>
2022-04-05 11:04:59 +0300

    added user authentication

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+// import { Http, Headers, RequestOptions, Response } from '@angular/http';
 @Injectable({
   providedIn: 'root'
 })
@@ -8,6 +9,14 @@ export class LoginService {
   url = 'http://127.0.0.1:9000/api/login'
   constructor(private http:HttpClient) { }
   login(credentials:any):Observable<any>{
-    return this.http.post<any>(this.url ,credentials, {withCredentials: true})
+    const httpOptions = {
+      withCredentials: true
+    };
+    return this.http.post<any>(this.url ,credentials,httpOptions)
+  }
+
+  getuser(token:any):Observable<any>{
+    let url = 'http://127.0.0.1:9000/api/user/'
+    return this.http.post(url, token)
   }
 }
