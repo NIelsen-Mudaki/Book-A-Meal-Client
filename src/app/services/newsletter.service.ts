069e9221b5995dc7229c6a7326b839cf3ef7088c
uhuru rawlings <rawlings.huru@student.moringaschool.com>
2022-04-05 08:52:56 +0300

    added signup to newslatetter

--- src/app/services/newsletter.service.ts
+++ src/app/services/newsletter.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
-
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
 export class NewsletterService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+  signupnewslatter(emailadress:any):Observable<any>{
+    let url = 'http://127.0.0.1:9000/api/newsletter/'
+    return this.http.post(url, emailadress)
+  }
 }
