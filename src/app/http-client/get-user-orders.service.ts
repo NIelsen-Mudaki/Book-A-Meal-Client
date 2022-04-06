b6e1d1838a7c25e587d61ad6cb536c7d231effab
Francis Githae <francis.githae@student.moringaschool.com>
2022-04-06 17:24:07 +0300

    display order historY

---
+++ src/app/http-client/get-user-orders.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+@Injectable({
+  providedIn: 'root'
+})
+export class GetUserOrdersService {
+  
+  constructor(private http:HttpClient) { }
+  
+public get_user_orders(userid:any):Observable<any>{
+  let url=`http://localhost:8000/api/user/2/orders/`
+  return this.http.get<any[]>(url)
+
+
+}
+}
--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { OrderService } from '../order-service/order.service';
+import { GetUserOrdersService } from '../http-client/get-user-orders.service';
+import { UserloginService } from '../services/userlogin.service';
+import { CookieService } from 'ngx-cookie-service';
+import { LoginService } from '../services/login.service';
 
 @Component({
   selector: 'app-order',
@@ -9,16 +13,49 @@ import { OrderService } from '../order-service/order.service';
 export class OrderComponent implements OnInit {
   orders: any
   id:any
-  constructor(private orderService:OrderService) { }
+  userOrders:any
+  user:any;
+  activeUser:any
+  user_obj:any;
+  constructor(private CookieService:CookieService,public LoginService:LoginService,private orderService:OrderService,private getUserOrdersService:GetUserOrdersService,private UserloginService:UserloginService) { }
 
   ngOnInit(): void {
-    this.getOrders()
+    this.getuser()
+    // this.getOrders()
+    this.getUserOrders()
   }
 
-  getOrders(){
-    this.orderService.getOrders(this.id).subscribe((data) =>{
-      this.orders = data
+  getuser(){
+    let user = this.CookieService.get('jwt')
+    let token = {
+      'jwt':user
+    }
+    this.LoginService.getuser(token).subscribe((data) => {
+      this.user_obj = data
       console.log(data)
+      this.setuser()
+    
     })
+}
+  setuser(){
+  this.UserloginService.loginuser(this.user_obj)
+  this.activeUser=this.UserloginService.user.id
+  }
+
+
+  getUserOrders(){
+
+      this.getUserOrdersService.get_user_orders(this.activeUser).subscribe((data)=>{
+
+        this.userOrders=data
+      });
+    
+    
+    // catch{
+
+    //   alert('please login')
+    // }
+
+
   }
 }
