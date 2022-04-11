1ed9fb5206823258336dbf44eb90910d31947c8d
Nielsen-Mudaki <jumbanielsen@gmail.com>
2022-04-11 23:11:58 +0300

    edit services

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,12 +1,15 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 // import { Http, Headers, RequestOptions, Response } from '@angular/http';
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
-  url = 'http://127.0.0.1:8000/api/login'
+  BASE_URL:string=environment.BASE_URL
+  url:string=`${this.BASE_URL}/api/login`
+
   constructor(private http:HttpClient) { }
   login(credentials:any):Observable<any>{
     const httpOptions = {
@@ -16,7 +19,7 @@ export class LoginService {
   }
 
   getuser(token:any):Observable<any>{
-    let url = 'http://127.0.0.1:8000/api/user/'
+    let url = `${this.BASE_URL}/api/user/`
     return this.http.post(url, token)
   }
 }
--- src/app/services/menu-item.service.ts
+++ src/app/services/menu-item.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 @Injectable({
   providedIn: 'root'
 })
 export class MenuItemService {
-   url = 'http://127.0.0.1:8000/api/getfiewmenu/'
+  BASE_URL:string=environment.BASE_URL
+  url:string=`${this.BASE_URL}/api/getfiewmenu/`
   constructor(private http:HttpClient) { }
   getmenu():Observable<any>{
     return this.http.get(this.url)
